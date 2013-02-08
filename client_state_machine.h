d2312d282b7a1cbc0cb107cdc2e3f2887ea19cb8
Matthew McFadden <mcfadden.matthew@gmail.com>
2013-02-08 16:54:27 -0600

    finished ana/matt merge

--- client_state_machine.h
+++ client_state_machine.h
@@ -26,12 +26,8 @@ client_state_machine* start_csm(sockaddr_in address. const Server* server);
 void send_msg(LSPMssage message, client_state_machine csm);
 void wts_to_wtr(client_state_machine csm);
 void wtr_to_wts(client_state_machine csm);
-<<<<<<< HEAD
-void receive_msg(msg message, client_state_machine csm);
-
-#endif
-=======
 void receive_msg(LSPMssage message, client_state_machine csm);
 LSPMessage get_appropriate_ACK(LSPMssage message);
 LSPMessage createACK(int connid);
->>>>>>> working on csm
+
+#endif
