5ffd396793299abd0608aacaa679868681f36855
Ana Parra <anakp525@hotmail.com>
2013-02-11 20:53:30 -0800

    separated .c and .h files

--- queue.c
+++ queue.c
@@ -6,3 +6,21 @@ void push_back(LSPMessage* msg, queue_node* head){
 	node->next = head;
 	head = node;
 }
+
+int consume_next(LSPMessage* msg, queue_node* head){
+	if(head == NULL) {return -1;} //no messages in the inbox
+	else if(head->next == NULL){ //one message in the inbox
+		msg = head->msg;
+		free(head); //we no longer need this message
+		return 0;
+	} 
+	else{
+		queue_node* head_cpy = head; //copy head, as we will still have messages left afterwards
+		while(head_cpy->next->next != NULL){ //Find the second to last node
+			head_cpy = head_cpy->next;
+		}
+		msg = head_cpy->next->msg; //msg is the last node
+		free(head_cpy->next); //free the last node
+		return 0;
+	}
+}
